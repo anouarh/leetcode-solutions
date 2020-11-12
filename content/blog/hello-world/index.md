361693a33e03a5aa4625d4600b2d8f529d343475
Anouar <anouar.hilali1@gmail.com>
2020-11-12 14:59:43 +0100

    Add tags logic

--- content/blog/hello-world/index.md
+++ content/blog/hello-world/index.md
@@ -2,6 +2,7 @@
 title: First post
 date: "2020-11-11T14:19:47+00:00"
 description: "Decided to start a LeetCode blog to compile solutions in one place."
+tags: ["general"]
 ---
 
 This is my first post, as I was starting to up my LeetCode game, I decided to track my progress (and understanding solutions better) by compiling the solutions as articles for this blog for others to see and myself to go back to.
--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,9 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   // Define a template for blog post
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
 
+  // Template for tag
+  const tagTemplate = path.resolve("src/templates/tags.js")
+
   // Get all markdown blog posts sorted by date
   const result = await graphql(
     `
@@ -20,9 +23,18 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
             fields {
               slug
             }
+            frontmatter {
+              tags
+            }
           }
         }
       }
+      tagsGroup: allMarkdownRemark(limit: 2000) {
+        group(field: frontmatter___tags) {
+          fieldValue
+        }
+      }
+    }
     `
   )
 
@@ -52,6 +64,18 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           id: post.id,
           previousPostId,
           nextPostId,
+        },
+      })
+    })
+    // Extract tag data from query
+    const tags = result.data.tagsGroup.group
+    // Make tag pages
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+        component: tagTemplate,
+        context: {
+          tag: tag.fieldValue,
         },
       })
     })
